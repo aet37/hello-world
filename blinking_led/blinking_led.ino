9940b95e339f5489655cff4b6155584863d1ef81
aet37 <69984232+aet37@users.noreply.github.com>
2021-03-30 13:44:16 -0400

    add hello world microcontroller file

---
+++ blinking_led/blinking_led.ino
@@ -0,0 +1,26 @@
+int PIN_SW = 7;
+int PIN_LED = 8;
+
+void setup()
+{
+  /* Setup code
+   * PIN7 is INPUT (SW2)
+   * PIN8 is OUTPUT (LED)
+   */
+  pinMode(PIN_SW, INPUT);
+  pinMode(PIN_LED, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(PIN_LED, LOW);
+  
+  // Check if switch is pressed, and if it is, blink LED
+  if(digitalRead(PIN_SW) == HIGH)
+  {
+    digitalWrite(PIN_LED, HIGH);
+    delay(5);
+    digitalWrite(PIN_LED, LOW);
+    delay(5);
+  }
+}
